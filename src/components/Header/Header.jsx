181a04a337fc64555e493db0d7ce8258e0318668
Sakar Pudasaini <barcaboysakar@gmil.com>
2023-04-30 15:07:40 +0545

    getting started

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container"></div>
+    </section>
+  );
+};
+
+export default Header;
